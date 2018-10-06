ede88e749f4019b15028622a0b1fc3e328f9cf18
JvrSolis <jvrsolis@outlook.com>
2018-10-06 12:56:35 -0500

    First

---
+++ tests/IntegrationTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace JvrSolis\ArrayMacros\Test;
+
+class IntegrationTest extends TestCase
+{
+    /** @test */
+    public function test_setup()
+    {
+        return $this->setup();
+    }
+}
